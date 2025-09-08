215d4606d05303520dce3e546734ab2c85fdf2a4
Nelsoon-ds <davidgaming108@gmail.com>
2025-09-08 10:59:50 +0200

    Trinket exercises for class

---
+++ src/TrinketExercises/Tile.java
@@ -0,0 +1,39 @@
+package TrinketExercises;
+
+public class Tile {
+    char letter;
+    int value;
+
+    public static void main(String[] args) {
+        Tile t1 = new Tile();
+        t1.printTile(t1);
+        String s = t1.toString();
+        System.out.print(s);
+    }
+
+    public Tile(char letter, int value) {
+        letter = this.letter;
+        value = this.value;
+    }
+    public Tile() {
+        this.letter = 'Z';
+        this.value = 10;
+    }
+    public char getLetter( ) {
+        return this.letter;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+    public void printTile(Tile object) {
+        System.out.println(object.letter + " " + object.value);
+    }
+
+    public String toString() {
+        return String.format("Character: %c, Value: %d", this.letter, this.value);
+    }
+    public boolean equals(Tile that) {
+        return this.value == that.value && this.letter == that.letter;
+    }
+}
